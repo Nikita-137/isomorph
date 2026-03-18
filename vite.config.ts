9c11d5aa887cdc5c11983ae6ffd62ecd9708487b
Nikita-137 <nichita.tcacenco@isa.utm.md>
2026-03-18 22:29:52 +0200

    Uploading

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/isomoph-experiments/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
